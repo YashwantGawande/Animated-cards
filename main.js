d7e5bab13d5d41ed5102f572006960e761ab87e5
YashwantNG <gawandeyashwant7@gmail.com>
2023-02-10 22:24:43 +0530

    update

--- main.js
+++ main.js
@@ -3,4 +3,15 @@ const groups = document.getElementByClassName("card-group");
 const handleLoveClick = () => {
   //bump active index
   const nextIndex = activeIndex + 1 <= groups.length - 1 ? activeIndex + 1 : 0;
+  const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
+    nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+  //Active group becomes after
+  currentGroup.dataset.status = "after";
+  //next group becomes active
+  nextGroup.dataset.status = "becoming-active-from-before";
+
+  setTimeout(() => {
+    nextGroup.dataset.status = "active";
+    activeIndex = nextIndex;
+  });
 };
